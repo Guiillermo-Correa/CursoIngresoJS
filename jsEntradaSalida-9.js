931b0659eba61457716719d1dc753a965ac8b662
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-10 19:27:19 -0300

    9 echo y comentado

--- jsEntradaSalida-9.js
+++ jsEntradaSalida-9.js
@@ -4,19 +4,21 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-	var suledo;
 	var importe;
-
-	importe=document.getElementById('suledo').value;
+/*Lo que hago a continuacion es tomar un importe por ID y transformarlo
+a entero por parseInt()*/
+	importe=document.getElementById('sueldo').value;
 	
 	importe=parseInt(importe);
+/* a continuacion le hago 
+el aumento del 10%(el importe+ el * 1.1)
+*/
+	resultado=importe*1.1;	
 
-	resultado=importe*1.1;
-
-
-	document.getElementById('resultado').value=resultado;
-
+/* uso el doccument por que quiero que me aparezca el resultdo en el
+cuadro de texto
+*/
+document.getElementById('resultado').value=resultado;
 
 	
-
 }
