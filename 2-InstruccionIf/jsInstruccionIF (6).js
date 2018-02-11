39b50586ebc796410372112b5bfb86296d877b0d
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-10 23:23:43 -0300

    del 1 al 6 echos

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,25 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	 edad=document.getElementById('edad').value;
 
+
+	 if(edad>18)
+	 {
+	 	alert("Mayor de edad");
+	 }
+	 
+	 if(edad>13 && edad<17)
+	 {
+	 	alert("Adolescente");
+
+	 }
+
+	 if(edad<13)
+	 {
+	 	alert("Niño");
+	 }
 
 
 
