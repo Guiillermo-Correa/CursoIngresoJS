7ecbd9de9ffddd520038af3fea9c1826bdd1cdf7
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-10 21:45:40 -0300

    1, 2, 3 y 4 (solo el 4 esta comentado)
    &&( y)

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
 //tomo la edad  
-
+	var edad;
+	edad=document.getElementById('edad').value;
+/* el && signiificaria "y"
+ osea seria, edad entre 13 Y 17 años (inclusive)
+ (para el inclusive se usa el "=")
+ */
+	if(edad>13 && edad<=17)
+	{
+		alert("adolescente");
+	}
 
 
 }//FIN DE LA FUNCIÓN
