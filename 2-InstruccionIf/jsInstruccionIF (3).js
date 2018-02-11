e071c2685f640decd0c98da0dbeb68ec39de481e
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-10 21:18:23 -0300

    1, 2 y 3 echos sin comentar
    usamos el if y el else

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
 
+	if(edad>17)
+	{
+		alert("Mayor de edad");
 
+	}
+	else
+	{
+		alert("menor de edad");
+
+	}
+	
 }//FIN DE LA FUNCIÓN
