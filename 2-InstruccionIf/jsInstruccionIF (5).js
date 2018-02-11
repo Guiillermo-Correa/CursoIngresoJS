2ca43f276715da4e45dac40d8c5929be0bfb866e
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-10 22:54:45 -0300

    del 1 al 5 echos

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,25 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if(edad>13 && edad<17)
+	{
+
+		alert("no es adolescentes");
+	}
+
+
+	/* no funciona usando el !
+
+	if(!(edad>13 && edad <17))
+	{
+
+	alert("NO es adolescente");
+
+	}
+*/
 
 }//FIN DE LA FUNCIÓN
