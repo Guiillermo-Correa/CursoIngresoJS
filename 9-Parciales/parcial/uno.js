db2b6189a9192d1046178ef04e6b6ee79e316254
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-12 13:22:22 -0300

    parcial 1 echo

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,14 @@
 
 function Mostrar()
 {
+	var laBase;
+	var perimetro;
+
+	laBase=document.getElementById('laBase').value;
+	perimetro= laBase*4;
+
+	alert("El perimetro es:" + perimetro);
+
+	
 
 }
