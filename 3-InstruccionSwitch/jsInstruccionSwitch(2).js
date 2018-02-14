f22848d3941f4444abd2eb189adf4628ab0451c6
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-14 12:29:16 -0300

    1 y 2 echos

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,37 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+	
 
-alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case"Enero":
+		case"Febrero":
+		case"marzo":
+		case"Abril":
+		case"Mayo":
+		case"Junio":
+			alert("Falta para el invierno.");
+			break;
+
+		case"Julio":
+		case"Agosto":
+			alert("Abrigate que hace frio");
+			break;
+
+		case"Septiembre":
+		case"Octubre":
+		case"Noviembre":
+		case"Diciembre":
+			alert("Ya pasamos el invierno");
+			break;
+		
+	
+	}
 
 
 
 
-}//FIN DE LA FUNCIÓN
+}
+//FIN DE LA FUNCIÓN
