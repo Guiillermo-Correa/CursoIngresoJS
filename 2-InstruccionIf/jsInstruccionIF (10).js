79c43896d8f63bc7f22e7ed60cb2d31566b5e4e4
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-14 12:28:33 -0300

    del 1 al 10 echos

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,34 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+	nota=Math.floor(Math.random()*10) + 1;
+
+if(nota>8 && nota<11)
+{
+	alert("EXCELENTE");
+}
+
+if(nota>3 && nota<9)
+{
+	alert("Tu nota es" + "nota"+ "APROBO");
+}		
+if(nota<4)
+{
+	alert("Vamos, la proxima se puede");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
+	// revisar problemas linea 17
+	var edad;
+	var estadoCivil;
 
+	edad=document.getElementById('edad').value;
 	
+	estadoCivil=document.getElementById('estadoCivil').value;
 
+	if(edad<18 && estadoCivil!="Soltero")
+	{
+		console.info(" NO HACER NADA ");
+
+	}
+
+	if(edad>17 && estadoCivil="Soltero")
+	{
+		alert("Es soltero y no es menor");
+	}
 
 }//FIN DE LA FUNCIÓN
