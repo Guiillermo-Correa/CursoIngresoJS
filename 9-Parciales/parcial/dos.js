f1920183552da8d945ec675092212460c6a5162c
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-15 22:49:33 -0300

    1 y 2 echos

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
-  
+	 var importeInicial;
+	 var importeFinal;
+	 var importeIva=0.21;
+	importeInicial=parseInt(importeInicial);
+	
+	importeInicial=prompt("ingre su importe");
+	
+	importeFinal=importeInicial*(1+importeIva);
+
+	document.getElementById('importeFinal').value=importeFinal;
+
+
+	console.log(importeFinal);
+	
+
 }
