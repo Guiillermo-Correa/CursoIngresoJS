4df43cf47b977b373f9a7f73dc4d8b79be258c17
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-19 21:45:14 -0300

    1 al 4 echos

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,31 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var numeroFinal;
+
+	numeroUno=prompt("Ingrese su numemero");
+	numeroDos=prompt("Ingrese su numemero");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+		{
+			numeroFinal= numeroUno*numeroDos;
+		}
+
+	if(numeroUno>numeroDos)
+		{
+			numeroFinal=numeroUno-numeroDos;
+		}
+
+	else
+		{
+			numeroFinal=numeroUno+numeroDos;
+		}
+
+		console.log(numeroFinal);
+
 
 }
