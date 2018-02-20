0fc8a50923224a30e1d3d544a36bf92931d8d9ef
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-19 21:13:31 -0300

    1 al 3 echos

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+	largo=document.getElementById('alrgo').value;
+
+	ancho=document.getElementById('ancho').value;
+
+	largo=parseInt(largo);
+
+	ancho=parseInt(ancho);
+
+	perimetro=((largo*2) + (ancho*2));
+	// * 3 o + 3?
+	alambre= perimetro*3;
+
+	alert(" Se necesitan " + alambre + " metros de alambre ");
+
 
 }
