5c5dcf2bcd69d7c28f1a19124baf11f1e8c2715a
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-24 12:55:12 -0300

    terminar 6 y 8

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,63 @@
+// ingreso de alumnos, no se sabe cuantos hay. se pedira el nombre 
+//  se quiere saber cuantos alumnos ingrese   
+// hacer lo que falta 
+
 function Mostrar()
 {
+ 	var nombre;
+ 	var cantidad=0;
+	 var seguir="";
+	 var promedio;
+	 var acumulador=0;
+	 var numero;
+	 var sexo=0;
+	 var varones=0;
+	 var mujeres=0;
+	 
 
+ 	while(seguir!="no")
+ 	{
+
+	 	nombre=prompt("ingrese el nombre"); 
+		numero=prompt("ingrese la nota");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+	
+		while(numero<0 || numero >10)
+		{
+			numero=prompt("ingrese la nota");
+			numero=parseInt(numero);
+		}
+
+		sexo=prompt("ingrese f o m");
+
+		while(sexo!="f" && sexo!="m")
+		{
+		 	sexo=prompt("Error,ingrese f ó m .");
+
+		}
+	 	if(sexo=="m")
+	 	{
+	 		varones++;
+	 	}
+	 	
+	 	if(sexo=="f")
+
+	 	{
+	 		mujeres++;
+	 	}
+
+	
+		cantidad=cantidad+1;
+
+	 	seguir=prompt("NO para salir");
+ 	
+ 	}
+ 	
+ 	promedio=acumulador/cantidad;
+
+ 	document.write("<br> son " + cantidad + " alumnos ");
+ 	document.write( "<br> El promedio es: " + promedio);
+ 	document.write( "<br> la contidad de varones es " +  varones);
+ 	document.write( "<br> la cantidad de mujeres es " +  mujeres);
 }
--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,15 @@
 function Mostrar()
 {
+	var importeFinal;
+	var importeDeVentas;
+
+	importeDeVentas=prompt("ingrese el importe");
+
+	while(importeDeVentas)
+
+//Hacerlo para la clase que viene 
+
+
+
 
 }
