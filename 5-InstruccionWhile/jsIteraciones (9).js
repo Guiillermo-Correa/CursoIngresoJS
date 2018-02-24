2875e69393e8ebda8a1a9166039334bd73a3a38d
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-23 22:15:43 -0300

    revisar 7, 8 y 9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,35 @@
 function Mostrar()
 {
 
-	var contador=0;
-	// declarar variables
+var contador=0;
+	var maximo;
+	var minimo;
+	var numero;
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		contador++;
+		numero=prompt("ingrese el numero");
+		numero=parseInt(numero);
+		if(contador==0)
+		{
+			maximo=numero;
+			minimo=numero;
+		}else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero<minimo)
+			{
+				minimo= numero
+			}
+		}
+		numero=prompt("ponga si para continuar");	
 	}
-
 
 
 
