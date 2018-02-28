d4a0a0887b8291d1b0279d7375915adfb70148f2
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-28 11:52:34 -0300

    seguir

--- 9-Parciales/parcial2/5-switch.js
+++ 9-Parciales/parcial2/5-switch.js
@@ -1,7 +1,28 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 5-Switch");
+	var mes;
+
+	mes=prompt("Ingrese el mes");
+
+	switch(mes)
+	{
+		case"enero":
+		case"febrero":
+			alert("Veranito!!!!");
+			break;
+		default:
+		alert("extraño enero y febrero");
+
+	}	
+
+
+
+
+
+
+	//alert("Funciona 5-Switch");
 	
 }
 
+ 
