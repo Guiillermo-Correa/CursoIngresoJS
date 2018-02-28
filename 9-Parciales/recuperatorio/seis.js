ed75e6a0a2b5e386b0c7968c7abc6f0e29e87e34
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-27 21:49:43 -0300

    1 al 7 echos (revisar el 6)

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -1,4 +1,57 @@
 function Mostrar()
 {
+	var peso;
+	var menosPesado;
+	var masPesado;
+	var contador = 0;
+	
+	peso = prompt("Ingrese el peso en kilos: ");
+	peso = parseInt(peso);
+
+	contador++;
+
+	while (contador>50 || contador<0)
+	{	
+		peso = prompt("Eror Ingrese denuevo el peso ");
+	 	peso = parseInt(peso);
+
+		while(contador>0)
+		{
+		
+		if (contador)
+			{
+				masPesado = peso;
+				menosPesado = peso;
+			}
+
+			if (contador == 1)
+			{
+				masPesado = peso;
+				menosPesado = peso;
+			}
+		
+		else
+			{
+					
+				if (peso > masPesado) 
+				{
+						
+						masPesado = peso; 
+				}
+				
+				if (peso < menosPesado) 
+				{
+						
+						menosPesado = peso;
+				}
+			}	
+		}
+
+		
+		
+	}
+
+	alert("El mas pesado del deposito es : " + masPesado);
+	alert("El menos pesado del deposito es : " + menosPesado);
 
 }
--- 9-Parciales/recuperatorio/siete.js
+++ 9-Parciales/recuperatorio/siete.js
@@ -1,4 +1,57 @@
 function Mostrar()
 {
+	var edad;
+	var sexo;
+	var promedio;
+	var edadMenor;
+	var contador = 0;
+	var sumaEdades = 0;
+	var contadorVaronesEdadMayor = 0;
+	var respuesta="si";
+	
+	while (respuesta=="si" && contador < 100)
+	{
+		edad = prompt("Ingrese la edad: ","0");
+		edad = parseInt(edad);
 
+		while (edad < 0 || edad > 100 || isNaN(edad))
+		{
+			edad = prompt("ErrorVuelva a ingresar la edad, entre 0 y 100: ");
+			edad = parseInt(edad);
+		}
+		contador++;
+		sumaEdades = sumaEdades + edad;
+		
+		sexo = prompt("Ingrese el sexo: ","f/m");
+
+		while (sexo !== "f" && sexo !== "m" || !isNaN(sexo))
+		{
+			sexo = prompt("Eror,Vuelva a ingresar el sexo, 'f' o 'm': ");
+		}
+
+		if (contador == 1) 
+		{
+			edadMenor = edad;
+		}
+		else 
+		{
+			if (edad < edadMenor) 
+			{
+				edadMenor = edad;
+			}
+		}
+
+		if (sexo == "m" && edad >= 20) 
+		{
+			contadorVaronesEdadMayor++;
+		}
+		respuesta=prompt("ponga si para poner otro numero");
+
+	}
+	
+	promedio = sumaEdades / contador;
+ 
+	alert("El promedio de edades es: " + promedio);
+	alert("La edad más baja es: " + edadMenor);
+	alert("La cantidad de varones con edad mayor o igual a 20 es: " + contadorVaronesEdadMayor);
 }
