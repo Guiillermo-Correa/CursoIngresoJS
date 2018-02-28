3884ccb5f1bc1ef78c7af7044d4cfb554427e303
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-02-28 11:55:02 -0300

    4 y 8 (revisar)

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,36 +1,57 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+no se valida y no se usa whilesolo IF.
+si ingreso dos numeros, si son iguales se concatenan 
+si el mayor es el primero se multiplica de lo contrario se restan
+si es el resultado de la multiplicacion es par, poner que es par
+
+*/
+
 function Mostrar()
 {
-	var numeroUno;
+	var	numeroUno;
 	var numeroDos;
-	var numeroFinal;
+	var resultado;
+	var par;
 
-	numeroUno=prompt("Ingrese el primer numero");
-	numeroDos=prompt("Ingrese el segundo numero");
+
+	numeroUno=prompt("Ingrese su numemero");
+	numeroDos=prompt("Ingrese su numemero");
 
 	numeroUno=parseInt(numeroUno);
 	numeroDos=parseInt(numeroDos);
 
-	
 	if(numeroUno==numeroDos)
-		{
-			numeroFinal= numeroUno*numeroDos;
-			document.write("El  numero final es :" + numeroFinal);
-		}
 
+	{
+			resultado=numeroUno+""+numeroDos;
+
+	}
 	if(numeroUno>numeroDos)
-		{
-			numeroFinal=numeroUno-numeroDos;
-			document.write("El  numero final es :" + numeroFinal);
-		}
+	
+	{
+			resultado=numeroUno*numeroDos;
 
+	}
+	
 	if(numeroUno<numeroDos)
-		{
-			numeroFinal=numeroUno+numeroDos;
-			document.write("El  numero final es :" + numeroFinal);
-		}
-		 
 
-		
+	{
+			resultado=numeroUno-numeroDos;
+
+	}
+
+	if(resultado%2==0 && resultado!=0)
+		{
+			resultado=resultado+" es par";
+		}
+	
+	document.write("<br> El numero es:" + resultado);
+
+
+
+
+
+	//alert("Funciona 4-if");
+	
 }
 
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,36 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+	se ingresa hasta que usuario quiera el nombre de un animal, el peso del mismo validar que sea mayor a 0
+	y la temperatura del habitad de este animal, validad entre -40 y +40
+	informar la acntidad de temperaturas pares, la cantidad de temp impar
+	el nombre del animal mas pesado
+	el nombre del animal menos pesado
+	la cantidad de animales que viven en habitas menores a 0 inclusive 
+	el promedio del peso de todos los animales 
+	la temperatura maxima y minima 
+*/
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	var nombreAnimal;
+	var pesoAnimal;
+	var temperatura;
+	var temperaturaMaxima
+	var temperaturaMinima;
+	var temperaturaPares;
+	var temperaturaImpares;
+	var animalMasPesado;
+	var animalMenosPesado;
+	var promedio;
+	var respuesta="si";
+		
+	nombreAnimal=prompt("Ingrese el nombre del animal");
+	pesoAnimal=prompt("Ingrese el peso del animal");
+	temperatura=prompt("Ingrese la temperatura");
+
+	
+
+
+
+//alert("Funciona 8-iteraciones");
 	
 }
 
